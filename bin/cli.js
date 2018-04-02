2c01c6df7554d22fedd7c5d49834076a64b62848
Joshua Wiens <joshuaw@easymetrics.com>
2018-04-02 15:42:32 -0500

    feat: Accept arguments for pacakge & tags

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 // This file has to exist as npm will try to chmod it when installing.
-require('../lib');
+const argv = require('yargs').argv;
+
+const compVer = require('../lib');
+
+compVer.compare(argv.name, argv.gte, argv.lt);
